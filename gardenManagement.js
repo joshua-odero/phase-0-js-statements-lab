30a64f813ba6f301861795de6cf30f82f6715e40
joshua-odero <misikojodero@gmail.com>
2025-11-17 18:57:42 +0300

    First commit: Modifying gardenManagement.js

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,31 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+
+if(temperature > 80){
+    //Do something if temperature is above 80.
+    console.log("Watering on");
+
+}else{
+    //Do something if the temperature is equal to or below 80.
+    console.log("Watering off");
+
+}
+
+if(timeOfDay === "evening" || timeOfDay === "night"){
+    //Do something if the time of the day is 'evening' or 'night'
+    console.log("Lights on");
+
+}else{
+    //Do something if the time of the day is neither 'evening' nor 'night'
+    console.log("Lights off");
+
+}
+
+while(soilMoisture <= 40){//conditional loop to do something on condition that the soil moisture is less than or equal to 40
+
+    console.log("The soil moisture is:" + soilMoisture);//console.log() prints the soil moisture
+    soilMoisture = soilMoisture + 5;//update the soil moisture by incrementing it by 5
+
+}
